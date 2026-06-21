30bf42aa2ed3fb028efa9444993949972bf5c91f
malinga-PW <pixelwave.lk@gmail.com>
2026-06-21 19:33:27 +0530

    Refactor next.config.mjs for clarity and structure

--- next.config.mjs
+++ next.config.mjs
@@ -6,39 +6,47 @@ const __dirname = path.dirname(__filename)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: {
-    root: __dirname,
-  },
-  reactStrictMode: false,
-  images: {
-    minimumCacheTTL: 2678400 * 6, // 3 months
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'a0.muscache.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.gstatic.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+    turbopack: {
+          root: __dirname,
+    },
+    reactStrictMode: false,
+    images: {
+          minimumCacheTTL: 2678400 * 6, // 3 months
+          remotePatterns: [
+            {
+                      protocol: 'https',
+                      hostname: 'images.pexels.com',
+                      port: '',
+                      pathname: '/**',
+            },
+            {
+                      protocol: 'https',
+                      hostname: 'images.unsplash.com',
+                      port: '',
+                      pathname: '/**',
+            },
+            {
+                      protocol: 'https',
+                      hostname: 'a0.muscache.com',
+                      port: '',
+                      pathname: '/**',
+            },
+            {
+                      protocol: 'https',
+                      hostname: 'www.gstatic.com',
+                      port: '',
+                      pathname: '/**',
+            },
+                ],
+    },
+    async rewrites() {
+          return [
+            {
+                      source: '/supabase-api/:path*',
+                      destination: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/:path*`,
+            },
+                ]
+    },
 }
 
 export default nextConfig
